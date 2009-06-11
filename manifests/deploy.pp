5dde423179201c38bab213696452d03a688ed96d
mh <mh@immerda.ch>
2009-06-12 00:21:15 +0200

    extraced everything in its own file

---
+++ manifests/deploy.pp
@@ -0,0 +1,40 @@
+define securefile::deploy(
+    $source,
+    $path = 'absent',
+    $owner = 'root',
+    $group = '0',
+    $mode = '0640'
+){
+    include ::securefile
+
+    if $require {
+        $real_require  = [ File['/e/.issecure'], $require]
+    } else {
+        $real_require =  File['/e/.issecure']
+    }
+
+    $real_path = $path ? {
+        'absent' => $name,
+        default => $path,
+    }
+
+    file{$name:
+        source => "puppet://$server/secfiles/$source",
+        path => "/e/$real_path",
+        owner => $owner,
+        group => $group,
+        mode => $mode,
+        ensure => present,
+        require => $real_require,
+    }
+    if $notify {
+        File[$name]{
+            notify => $notify,
+        }
+    }
+    if $before {
+        File[$name]{
+            before => $before,
+        }
+    }
+}
