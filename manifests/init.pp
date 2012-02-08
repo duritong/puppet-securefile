17a331536b5111fd73dad70a2cc645cd4d089c92
mh <mh@immerda.ch>
2012-02-08 09:20:31 +0100

    move global variables to hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,3 @@
-# modules/securefile/manifests/init.pp - manage securefiles stuff
-# Copyright (C) 2007 admin@immerda.ch
-#
-
 class securefile {
 
   file{"/e":
@@ -9,69 +5,54 @@ class securefile {
     owner => root, group => 0, mode => 0755;
   }
 
-  if $selinux == 'true' {
+  if $::selinux == 'true' {
     File['/e']{
       seltype => 'cert_t'
     }
   }
 
   file{'/e/.issecure':
-    source  => "puppet:///modules/securefile/issecure",
+    content => "### file managed by puppet ####\n# this file should ensure that a crypted disk is mounted!\n# don't remove it\n",
     owner => root, group => 0, mode => 0644;
   }
 
   mount{'/e': }
-  if $e_mount_source != 'fake' {
-    $real_e_mount_source = $e_mount_source ? {
-      '' => "/dev/xvdb3",
-      default => $e_mount_source
-    }
-    $real_e_mount_fstype = $e_mount_fstype ? {
-      '' => $operatingsystem ? {
+  if hiera('e_mount_source','/dev/xvdb3') != 'fake' {
+    $def_fs =  $::operatingsystem ? {
         openbsd => 'ffs',
-        default => hiera('e_mount_fstype','ext3')
-      },
-      default => $e_mount_fstype
+        default => 'ext3'
     }
-    $real_e_mount_options = $e_mount_options ? {
-      '' => $operatingsystem ? {
-        openbsd => 'rw,nodev,nosuid',
-        default => 'nodev'
-      },
-      default => $e_mount_options,
+    $def_mount_options = $::operatingsystem ? {
+      openbsd => 'rw,nodev,nosuid',
+      default => 'nodev'
     }
 
     Mount['/e']{
-      device  => $real_e_mount_source,
+      device  => hiera('e_mount_source','/dev/xvdb3'),
       ensure  => mounted,
-      fstype  => $real_e_mount_fstype,
-      options => $real_e_mount_options,
-      remounts => $operatingsystem ? {
+      fstype  => hiera('e_mount_fstype',$def_fs),
+      options => hiera('e_mount_options',$def_mount_options),
+      remounts => $::operatingsystem ? {
         openbsd => false,
         default => true
       },
       require => File["/e"],
     }
 
-    case $operatingsystem {
-      openbsd: { info("openbsd doesn't yet support atboot") }
+    case $::operatingsystem {
+      openbsd: { }
       default: {
-        $real_e_mount_atboot = $e_mount_atboot ? {
-          '' => true,
-          default => $e_mount_atboot
-        }
-
         Mount['/e']{
-          atboot  => $real_e_mount_atboot,
+          atboot  => hiera('e_mount_atboot',true)
         }
-       }
+      }
     }
     File['/e/.issecure']{
       require => Mount["/e"]
     }
   } else {
     Mount['/e']{
-    ensure => absent
+      ensure => absent
     }
   }
 }
